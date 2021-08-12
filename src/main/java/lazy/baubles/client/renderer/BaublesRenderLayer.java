ab945d2689b90d2c537e42902b4e8638ac3a7672
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-08-12 11:30:57 +0100

    Fixed wrong field name and using the right way to rotation using pose

--- src/main/java/lazy/baubles/client/renderer/BaublesRenderLayer.java
+++ src/main/java/lazy/baubles/client/renderer/BaublesRenderLayer.java
@@ -1,6 +1,7 @@
 package lazy.baubles.client.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Vector3f;
 import lazy.baubles.api.bauble.IBaublesItemHandler;
 import lazy.baubles.api.cap.CapabilityBaubles;
 import lazy.baubles.api.render.IRenderBauble;
@@ -13,6 +14,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 import org.lwjgl.opengl.GL11;
 
@@ -30,17 +32,19 @@ public class BaublesRenderLayer<T extends Player, M extends PlayerModel<T>> exte
             return;
 
         player.getCapability(CapabilityBaubles.BAUBLES).ifPresent(inv -> {
-            dispatchRenders(matrixStack, inv, player, IRenderBauble.RenderType.BODY, partialTicks);
+
+            this.dispatchRenders(matrixStack, inv, player, IRenderBauble.RenderType.BODY, partialTicks);
 
             float yaw = player.yHeadRotO + (player.yHeadRot - player.yHeadRotO) * partialTicks;
             float yawOffset = player.yBodyRotO + (player.yBodyRot - player.yBodyRotO) * partialTicks;
-            float pitch = ObfuscationReflectionHelper.getPrivateValue(Entity.class, player, "xRot");
+            float xRot = ObfuscationReflectionHelper.getPrivateValue(Entity.class, player, "f_19858_");
+            float pitch = player.xRotO + (xRot - player.xRotO) * partialTicks;
 
             matrixStack.pushPose();
-            GL11.glRotatef(yawOffset, 0, -1, 0);
-            GL11.glRotatef(yaw - 270, 0, 1, 0);
-            GL11.glRotatef(pitch, 0, 0, 1);
-            dispatchRenders(matrixStack, inv, player, IRenderBauble.RenderType.HEAD, partialTicks);
+            matrixStack.mulPose(Vector3f.YN.rotationDegrees(yawOffset));
+            matrixStack.mulPose(Vector3f.YP.rotationDegrees(yaw - 270));
+            matrixStack.mulPose(Vector3f.ZP.rotationDegrees(pitch));
+            this.dispatchRenders(matrixStack, inv, player, IRenderBauble.RenderType.HEAD, partialTicks);
             matrixStack.popPose();
         });
     }
@@ -50,11 +54,11 @@ public class BaublesRenderLayer<T extends Player, M extends PlayerModel<T>> exte
             ItemStack stack = inv.getStackInSlot(i);
             if (!stack.isEmpty()) {
                 stack.getCapability(CapabilityBaubles.ITEM_BAUBLE).ifPresent(bauble -> {
-                    if (bauble instanceof IRenderBauble) {
+                    if (bauble instanceof IRenderBauble rb) {
                         poseStack.pushPose();
                         GL11.glColor3ub((byte) 255, (byte) 255, (byte) 255);
                         GL11.glColor4f(1F, 1F, 1F, 1F);
-                        ((IRenderBauble) bauble).onPlayerBaubleRender(player, type, partialTicks);
+                        rb.onPlayerBaubleRender(player, type, partialTicks);
                         poseStack.popPose();
                     }
                 });
