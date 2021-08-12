9a492fdec8a7df6bd24569aee0eb03e7d8e5fc4e
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-08-12 11:30:18 +0100

    Add test item

--- src/main/java/lazy/baubles/setup/ModItems.java
+++ src/main/java/lazy/baubles/setup/ModItems.java
@@ -1,9 +1,15 @@
 package lazy.baubles.setup;
 
 import lazy.baubles.api.BaublesAPI;
+import lazy.baubles.api.bauble.BaubleType;
+import lazy.baubles.api.bauble.IBauble;
+import lazy.baubles.api.render.IRenderBauble;
 import lazy.baubles.datadriven.BaubleJson;
 import lazy.baubles.datadriven.BaubleJsonItem;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,6 +21,25 @@ public class ModItems {
     public static void init() {
         if (!BaubleJson.loadBaubles().isEmpty())
             ITEMS.register("bauble", () -> new BaubleJsonItem(BaubleJson.loadBaubles()));
+
+        ITEMS.register("test_bauble", TestItem::new);
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    public static class TestItem extends Item implements IBauble, IRenderBauble {
+
+        public TestItem() {
+            super(new Properties().tab(CreativeModeTab.TAB_FOOD));
+        }
+
+        @Override
+        public BaubleType getBaubleType(ItemStack stack) {
+            return BaubleType.TRINKET;
+        }
+
+        @Override
+        public void onPlayerBaubleRender(Player player, RenderType type, float partialTicks) {
+            System.out.println(player.getName());
+        }
     }
 }
